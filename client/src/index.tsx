9a7ecc5d9d0c4226a5f83e208f223b7ad4b29fc5
tnkalive <42827461+tnkalive@users.noreply.github.com>
2020-10-02 21:58:21 +0700

    change index.ts of page to page.ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import { Home, Header } from "./Page";
+import { Home, Header } from "./Page/Page";
 
 const App = () => {
    return (
